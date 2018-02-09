14c1dadfdc9fd4dcf65049fc9c0ad747a1cca7eb
zhaoyi <zyoric@foxmail.com>
2018-02-09 15:42:22 +0800

    添加socketio和elementUi

---
+++ src/components/main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <p>{{msg}}</p>
+    <el-button round @click="clickBtn">圆形按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg : 'Welcome to Your Vue.js App',
+      id : '1122'
+    }
+  },
+  sockets : {
+    connect: ()=>{
+      this.id = this.$socket.id;
+    },
+    customEmit:( val )=>{
+      console.log( 'customEmit a val : ' + val );
+    }
+  },
+  methods : {
+    clickBtn : ( val )=>{
+      console.log( this );
+      // this.$socket.emit( val );
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+p {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+// use socketio
+import VueSocketio from 'vue-socket.io';
+Vue.use( VueSocketio, 'http://socketserver.com:1923' ); 
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Element from 'element-ui';
+
+Vue.use(Element, { size: 'small' });
+Vue.use( ElementUI );
 
 Vue.config.productionTip = false
 
